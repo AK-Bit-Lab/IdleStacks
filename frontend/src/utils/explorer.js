4277592261f7c83b4c22fd4e76c06429bea78c45
AK-Bit-Lab <274833942+AK-Bit-Lab@users.noreply.github.com>
2026-06-22 22:12:37 +0100

    feat: add Stacks explorer url utilities

---
+++ frontend/src/utils/explorer.js
@@ -0,0 +1,14 @@
+import { APP_NETWORK } from './constants';
+
+export const getExplorerUrl = (txId) => {
+  const isTestnet = APP_NETWORK.isMainnet() === false;
+  const chain = isTestnet ? 'testnet' : 'mainnet';
+  const cleanTxId = txId.startsWith('0x') ? txId : \`0x\${txId}\`;
+  return \`https://explorer.hiro.so/txid/\${cleanTxId}?chain=\${chain}\`;
+};
+
+export const getAddressUrl = (address) => {
+  const isTestnet = APP_NETWORK.isMainnet() === false;
+  const chain = isTestnet ? 'testnet' : 'mainnet';
+  return \`https://explorer.hiro.so/address/\${address}?chain=\${chain}\`;
+};
